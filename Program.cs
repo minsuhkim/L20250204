26a6b1c297cdab43324969831aeedbc191d9597a
minsuhkim <124859537+minsuhkim@users.noreply.github.com>
2025-02-04 09:34:45 +0900

    중복 체크 부분 다른 방법들 주석

--- Program.cs
+++ Program.cs
@@ -6,24 +6,28 @@
         {
             int size = 52;
             int[] numbers = new int[size];
-            System.Random rnd = new System.Random();
+            Random rnd = new Random();
 
             for (int i = 0; i < numbers.Length; i++)
             {
-                numbers[i] = i+1;
+                numbers[i] = i + 1;
             }
 
             int[] rndNums = new int[8];
             int index = 0;
-            for(int i=0; i<8; i++)
+            for (int i = 0; i < 8; i++)
             {
                 //랜덤한 정수 생성
                 int rndNum = numbers[rnd.Next(numbers.Length)];
                 //중복 체크
+                /*
+                 * 최적화 >> 이중 반복으로 중복을 체크하는 것보다는 중복 체크용 bool 배열을 하나 만들어서 체크하는 게 시간이 덜 걸릴 듯 함
+                 *        >> Random.Shuffle 함수를 사용해서 배열을 섞어서 출력하는 방법도 있음
+                 */
                 bool check = false;
-                for(int j=0; j<index; j++)
+                for (int j = 0; j < index; j++)
                 {
-                    if(rndNums[j] == rndNum)
+                    if (rndNums[j] == rndNum)
                     {
                         check = true;
                         break;
@@ -41,7 +45,7 @@
                 }
             }
             //출력
-            for(int i=0; i<8;i++)
+            for (int i = 0; i < 8; i++)
             {
                 Console.WriteLine(rndNums[i]);
             }
